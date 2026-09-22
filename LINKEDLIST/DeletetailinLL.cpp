30c883e75386b3743baa71dd9b389604da0ced69
saisuukaanyaa-dev <saisuukaanyaa@gmail.com>
2026-09-22 15:41:07 +0530

    Add delete tail in linked list

--- LINKEDLIST/DeletetailinLL.cpp
+++ LINKEDLIST/DeletetailinLL.cpp
@@ -6,43 +6,56 @@ public:
     int data;
     Node* next;
 
-    Node(int data1, Node* next1){
-        data = data1;
-        next = next1;
-    }
-
     Node(int data1){
         data = data1;
         next = nullptr;
     }
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
 };
 
 Node* convertArray2LL(vector<int> arr){
+
     Node* head = new Node(arr[0]);
     Node* mover = head;
 
     for(int i = 1; i < arr.size(); i++){
         Node* temp = new Node(arr[i]);
         mover->next = temp;
-        mover = mover->next;
+        mover = temp;
     }
 
     return head;
 }
 
-Node* removeHead(Node* head){
+Node* removeTail(Node* head){
+
     if(head == nullptr){
         return nullptr;
     }
 
+    if(head->next == nullptr){
+        delete head;
+        return nullptr;
+    }
+
     Node* temp = head;
-    head = head->next;
-    delete temp;
+
+    while(temp->next->next != nullptr){
+        temp = temp->next;
+    }
+
+    delete temp->next;
+    temp->next = nullptr;
 
     return head;
 }
 
 void print(Node* head){
+
     Node* temp = head;
 
     while(temp != nullptr){
@@ -54,11 +67,12 @@ void print(Node* head){
 }
 
 int main(){
+
     vector<int> arr = {2,4,5,8};
 
     Node* head = convertArray2LL(arr);
 
-    head = removeHead(head);
+    head = removeTail(head);
 
     print(head);
 }
